2e1fe2f04a147dba6749ffc05ca9505acbd96591
unknown <mannisingh182@gmail.com>
2021-02-19 19:33:39 -0800

    Updates

---
+++ precache-manifest.0c4b9c7b6ee2b6b14307281ae20bff40.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a4fee2692a6555bfbde19037aa1eb27",
+    "url": "/Maneet79/Security-Websiteindex.html"
+  },
+  {
+    "revision": "27bce3c55d1a4008259a",
+    "url": "/Maneet79/Security-Websitestatic/css/main.ac5ab54c.chunk.css"
+  },
+  {
+    "revision": "9efaa613b976a6609c34",
+    "url": "/Maneet79/Security-Websitestatic/js/2.9791d0c0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Maneet79/Security-Websitestatic/js/2.9791d0c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27bce3c55d1a4008259a",
+    "url": "/Maneet79/Security-Websitestatic/js/main.f3064079.chunk.js"
+  },
+  {
+    "revision": "ee8fa5f5e83efc3cf103",
+    "url": "/Maneet79/Security-Websitestatic/js/runtime-main.a20766a9.js"
+  },
+  {
+    "revision": "4fad3374d5c4dad8375e4606c9595450",
+    "url": "/Maneet79/Security-Websitestatic/media/3740777.4fad3374.jpg"
+  },
+  {
+    "revision": "57f90b8ab958d947b6f662e79e8adfba",
+    "url": "/Maneet79/Security-Websitestatic/media/comp-logo.57f90b8a.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Maneet79/Security-Websitestatic/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d3b2f0c161e3c723f77a5157f826ae74",
+    "url": "/Maneet79/Security-Websitestatic/media/police.d3b2f0c1.jpg"
+  }
+]);
